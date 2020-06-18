6bd630c84d783a164126de32d56804660df0e85d
Pedro Lucas <pedro.lucaswpl@gmail.com>
2020-06-18 14:26:44 -0300

    usando a função unlink

---
+++ UNLINK/exemplo-02.php
@@ -0,0 +1,14 @@
+<?php
+
+    $dir = "images";
+    if(!is_dir($dir)) mkdir($dir);
+
+    foreach (scandir($dir) as $item) {
+        if(!in_array($item,array(".",".."))){
+            unlink($dir.DIRECTORY_SEPARATOR.$item);
+        }
+    }
+
+    echo "Execução realizada com sucesso.";
+
+?>
