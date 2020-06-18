96c5c90a29bff49fbf610204fd15d85ef621a877
Pedro Lucas <pedro.lucaswpl@gmail.com>
2020-06-18 15:39:21 -0300

    lendo conteúdo dos arquivos

---
+++ FGETS/exemplo-01.php
@@ -0,0 +1,32 @@
+<?php
+
+    $filename = "usuarios.csv";
+
+    //verificação se o arquivo existe ou não
+    if(file_exists($filename)){
+
+        $file = fopen ($filename, "r");
+    
+        //explode foi usado para retirar as , assim transformando o file em uma array
+        //pegou somente a primeira linha do arquivo que se tratava dos cabeçalhos
+        //fgets funciona pegando apenas uma linha por vez
+        $headers = explode(",", fgets($file));
+
+        $data = array();
+        while ($row = fgets($file)) {
+
+            $rowData = explode(",", $row); 
+            $linha = array();
+
+            for ($i=0; $i < count($headers); $i++) { 
+                $linha[$headers[$i]] = $rowData[$i];
+            }
+
+            array_push($data,$linha);
+        }
+
+        echo json_encode($data);
+        
+        fclose($file);
+    }
+?>
